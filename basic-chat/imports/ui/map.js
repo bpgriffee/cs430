0786358bd1fed6c0343116b79389d56c22037bab
brad <bpgriffee@mix.wvu.edu>
2017-04-23 15:55:05 -0400

    geo stuff

--- basic-chat/imports/ui/map.js
+++ basic-chat/imports/ui/map.js
@@ -17,12 +17,42 @@ Template.map.events({
 function initMap() {
   var GoogleMapsLoader = require('google-maps'); // only for common js environments
   var map;
+  var mynewlat = Locations.findOne({_id: Session.get("user_to_get").lat});
+  var mylat = 39.643217;
+  var mynewlng = Locations.findOne({_id: Session.get("user_to_get").long});
+  var mylng = -79.960964;
+  alert(mynewlat);
+  alert(mynewlng);
   GoogleMapsLoader.KEY = 'AIzaSyBhoQ8_9JlgO88J83OB4BJzxBGhIBV8opM&v=3.exp&';
   GoogleMapsLoader.load(function(google) {
     map = new google.maps.Map(document.getElementById('map'), {
       center: {lat: 39.643217, lng: -79.960964},
       zoom: 13
     });
+    var myLatLng = new google.maps.LatLng(mylat, mylng);
+    var marker = new google.maps.Marker({
+      position: myLatLng,
+      map: map,
+      animation: google.maps.Animation.DROP,
+    });
+    map.panTo(myLatLng);
   });
-  
 }
+
+function dropPoint(newLatLng) {
+
+
+        var infowindow = new google.maps.InfoWindow({
+            content: '<h3>Marker #'+markercount.toString()+'</h3>'+
+                     '<p>Population: '+population.toString()+'\b'+
+                     '<p>Pos: '+newLatLng.toString()+'</p>'
+        });
+        marker.addListener('click',function(){
+            infowindow.open(map,marker);
+        });
+        markercount++;
+// Need to add function here to hide/show markers/heatmap
+        map.panTo(newLatLng);
+        marker.setMap(map);
+        heatmap.setMap(map);
+    }
