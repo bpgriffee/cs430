c50854ac47b93351b8fd20781101285c58293a4a
JOSHNEWLAPTOP\Josh <jemcpherson@mix.wvu.edu>
2017-04-22 22:16:55 -0400

    Remove User works. A little glitchy when the user being removed is the current user.

--- basic-chat/imports/ui/user.js
+++ basic-chat/imports/ui/user.js
@@ -1,9 +1,20 @@
 import { Template } from 'meteor/templating';
 import { Session } from 'meteor/session';
+import { Groups } from '../api/groups.js';
 
 import './user.html';
 
+Template.user.helpers({
+
+  current_user_is_owner(){
+    return Session.get("Group").owner == Meteor.userId();
+  }
+});
+
 Template.user.events({
   'click .get-location'(){
+  },
+  'click .remove-user'(){
+        Groups.update({_id: Session.get("Group")._id}, {$pull: {users: this._id}});
   },
 })
--- basic-chat/server/main.js
+++ basic-chat/server/main.js
@@ -2,10 +2,8 @@ import { Accounts } from 'meteor/accounts-base';
 
 import '../imports/api/groups.js';
 import '../imports/api/messages.js';
-import '../imports/api/invites.js';
 
 Accounts.onCreateUser((options, user) => {
   user.username = user.emails[0].address;
-  user.profile.muted = [];
   return user;
 });
